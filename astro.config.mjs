d919261815f166a00ebe938f7621dc82fad31159
lidongxun967 <1985409711@qq.com>
2025-06-11 14:04:30 +0800

    优化配置文件格式，添加 Header 组件，支持社交图标和导航链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,17 +11,28 @@ export default defineConfig({
   integrations: [
     preact(),
     partytown(),
-    starlight({favicon: '/favicon.ico',
+    starlight({
+      favicon: "/favicon.ico",
       title: "Class Widgets",
       logo: { src: "./assets/images/favicon.png", alt: "Class Widgets Logo" },
-      sidebar: [{
-        label: "用户文档",
-        autogenerate: { directory: 'user_docs' },
-      },{
-        label: "开发者文档",
-        autogenerate: { directory: 'dev_docs' },
-      }],    components: {
-      Head: "./components/starlight/Head.astro"}
+      sidebar: [
+        {
+          label: "用户文档",
+          autogenerate: { directory: "user_docs" },
+        },
+        {
+          label: "开发者文档",
+          autogenerate: { directory: "dev_docs" },
+        },
+      ],
+      components: {
+        Head: "./components/starlight/Head.astro",
+        Header: "./components/starlight/Header.astro",
+      },
+      customCss: [
+        // 你的自定义 CSS 文件的相对路径
+        "./styles/global.css",
+      ],
     }),
   ],
   vite: {
